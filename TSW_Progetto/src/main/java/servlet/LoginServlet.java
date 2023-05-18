82b922c4e774490bcd7b5c11bf62bd4b449f15c0
DoroteaSerrelli <d.serrelli1@studenti.unisa.it>
2023-05-18 13:00:51 +0200

    progress

--- TSW_Progetto/src/main/java/servlet/LoginServlet.java
+++ TSW_Progetto/src/main/java/servlet/LoginServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,6 +17,13 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>"+username+" "+ password+"</title></head>");
+		out.println("<body</body>");
+		
 		response.setContentType("text/html");
 		doGet(request, response);
 	}
